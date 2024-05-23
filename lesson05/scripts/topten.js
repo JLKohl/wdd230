375aa426e2f7c790289505f3466defa6a37834a8
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-05-23 16:44:55 -0400

    made a checkmark and read button :)

--- lesson05/scripts/topten.js
+++ lesson05/scripts/topten.js
@@ -3,28 +3,34 @@ const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
 button.addEventListener('click', function(){
-    do{
-        if (input.value != ''){
-            const li  = document.createElement('li');
-            const deleteButton = document.createElement('button');
-            li.textContent = input.value;
-            deleteButton.textContent = 'x';
-            li.append(deleteButton);
-            list.append(li);
 
-            deleteButton.addEventListener('click', function (){
-                list.removeChild(li);
-                input.focus();
-            });
+    if (input.value != ''){
+        const li  = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        const checkMark = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '✖️';
+        checkMark.textContent = '✔️';
+        li.append(checkMark);
+        li.append(deleteButton);
+        list.append(li);
 
+        deleteButton.addEventListener('click', function (){
+            list.removeChild(li);
             input.focus();
+        });
 
-            input.value = '';
+        checkMark.addEventListener('click', function(){
+            this.innerText = 'READ'
+        });
 
-        } else {
-            alert('please enter a book and chapter');
-        }
- 
-    } while (i <= 10)
+        input.focus();
+
+        input.value = '';
+
+    } else {
+        alert('please enter a book and chapter');
+    }
+
 
 });
