bf7ebad96c377392dce5b1feea1a85a5fa60448f
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-06-20 11:59:44 -0500

    finished project

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,31 @@ const linksURL = "https://jlkohl.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
+}
+
+
+const displayLinks = (weeks) => {
+
+    const cardElement = document.querySelector(".card");
+
+    weeks.lesson.forEach((week) => {
+        let card = document.createElement("div");
+
+        let lesson = document.createElement("h3");
+        lesson.textContent = ` Lesson ${week.lesson} `;
+        card.appendChild(lesson);
+        
+        week.links.forEach((link) => {
+            let anchor = document.createElement("a");
+            anchor.href = baseURL + link.url;
+            anchor.textContent = ` - ${link.title} `;
+            lesson.appendChild(anchor)
+        });
+        
+        cardElement.appendChild(card)
+    });
+
 }
 
 getLinks();
