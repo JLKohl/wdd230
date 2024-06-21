fd513d7931f6a63df2dff562c1bd91746b0867c5
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-06-21 14:59:12 -0500

    trying to fix images

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -8,10 +8,11 @@ async function getMembers() {
 }
 const displayMembers = (members) => {
 
-    const cardElement = document.querySelector(".card");
+    const gridElement = document.querySelector(".grid");
 
     members.forEach((member) => {
         const card = document.createElement("div");
+        card.className ="card";
         
         const img = document.createElement("img");
         console.log("Image source: " + member.icon);
@@ -21,8 +22,29 @@ const displayMembers = (members) => {
         const h1 = document.createElement("h1");
         h1.textContent = member.companyName;
         card.appendChild(h1);
-
-        cardElement.appendChild(card)
+        
+        const address = document.createElement("p");
+        address.textContent = member.address;
+        card.appendChild(address);
+        
+        const phoneNumber = document.createElement("p");
+        phoneNumber.textContent = member.phoneNumber;
+        card.appendChild(phoneNumber);
+        
+        const website = document.createElement("a");
+        website.textContent = member.website;
+        website.href = member.website;
+        card.appendChild(website);
+        
+        const description = document.createElement("p");
+        description.textContent = member.description;
+        card.appendChild(description);
+        
+        const membershipLevel = document.createElement("p");
+        membershipLevel.textContent =`Membership Level: ${member.membershipLevel}`;
+        card.appendChild(membershipLevel);
+        
+        gridElement.appendChild(card)
     });
 
 }
