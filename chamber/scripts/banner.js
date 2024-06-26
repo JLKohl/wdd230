0dc77928e9f771076354b332d0603e25bcd85af6
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-06-26 13:35:14 -0500

    working on closing banner

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -3,4 +3,12 @@ let day = today.getDay();
 
 if (day === 1 || day === 3 || day === 5) {
     document.getElementById('special-banner').style.display = 'block';
+} else {
+    document.getElementById('special-banner').style.display = 'none';
 }
+
+
+document.getElementById('close-banner').addEventListener('click',
+function() {
+    document.getElementById('special-banner').display = `none`;
+});
